29ba96eb56d3b820aa1580eab22d643ea9e77d2a
Kushagra Saxena <kushagrasaxena061@gmail.com>
2024-06-18 03:33:51 +0530

    styling complete

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Kushagra's Portfolio",
+  description: "Portfolio Website",
 };
 
 export default function RootLayout({
